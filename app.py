18bceb2f9cda64f8e34c0de8016193d40562b47b
dragonstratagem-blip <dragonstratagem@gmail.com>
2026-06-03 12:41:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.set_page_config(layout="wide")
 # --- Custom CSS Styling ---
 st.markdown("""
 <style>
-div.stButton > button { font-size: 80px !important; padding: 1px 2px !important; width: 150%; }
+div.stButton > button { font-size: 800px !important; padding: 100px 200px !important; width: 150%; }
 div.stButton:nth-of-type(1) > button { background-color: #FFD700 !important; color: #8B4513 !important; }
 div.stButton:nth-of-type(2) > button { background-color: #C0C0C0 !important; color: #000000 !important; }
 </style>
